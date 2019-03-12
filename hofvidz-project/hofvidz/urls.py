2e13009bcc2ff24b0e7c0a1b26e0053f28f8d02a
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-03-12 17:19:41 -0500

    Testing css

--- hofvidz-project/hofvidz/urls.py
+++ hofvidz-project/hofvidz/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
     path('accounts', include('django.contrib.auth.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
