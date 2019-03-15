07ec19985fcc2258cd0ba36e4ed62ea2a3e035f5
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-03-15 16:01:57 -0500

    migrate halls

--- hofvidz-project/halls/models.py
+++ hofvidz-project/halls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Hall(models.Model):
+    title = models.CharField(max_length=257)
+
+class Video(models.Model):
+    title = models.CharField(max_length=257)
+    url = models.URLField()
+    youtube_id=models.CharField(max_length=257)
+    hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
--- hofvidz-project/hofvidz/settings.py
+++ hofvidz-project/hofvidz/settings.py
@@ -123,3 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
