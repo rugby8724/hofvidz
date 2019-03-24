c7bd08677824227dbc587bed31eb13bd066a08b3
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-03-23 19:39:52 -0500

    Work on Home Page

--- hofvidz-project/hofvidz/views.py
+++ hofvidz-project/hofvidz/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
+from halls.models import Hall, Video
 
 class HomePage(TemplateView):
+    model = Hall
+    rencent_halls = model.objects.all().order_by('-id')[:3]
+    popular_halls = [model.objects.get(pk=2),model.objects.get(pk=7),model.objects.get(pk=5)]
     template_name = 'index.html'
